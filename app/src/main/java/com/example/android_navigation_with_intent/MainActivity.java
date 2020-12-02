ccf9ba37cef2da83035022dea2f4b51e14f2b43c
LUXIANZE <luxianze98@gmail.com>
2020-12-03 03:53:50 +0800

    Completed functionalities for RelativeLayout

--- app/src/main/java/com/example/android_navigation_with_intent/MainActivity.java
+++ app/src/main/java/com/example/android_navigation_with_intent/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android_navigation_with_intent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            if(resultCode == RESULT_OK){
+                // Get Result from previous intent
+                String userName = data.getStringExtra("userName");
+
+                // Toast Presets
+                Context context = getApplicationContext();
+                CharSequence text = "Welcome Back, " + userName + "!";
+                int duration = Toast.LENGTH_SHORT;
+
+                // launch Toast
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }
+    }
+
+    public void RelativeLayoutBtnClicked(View view) {
+        Intent navigateToRelativeLayout = new Intent(this, RelativeLayout.class);
+        startActivityForResult(navigateToRelativeLayout, 1);
+    }
+    public void ConstraintLayoutBtnClicked(View view) {
+        Intent navigateToConstraintLayout = new Intent(this, ConstraintLayout.class);
+        startActivity(navigateToConstraintLayout);
+    }
+    public void GridLayoutBtnClicked(View view) {
+        Intent navigateToGridLayout = new Intent(this, GridLayout.class);
+        startActivity(navigateToGridLayout);
     }
 }
--- app/src/main/java/com/example/android_navigation_with_intent/RelativeLayout.java
+++ app/src/main/java/com/example/android_navigation_with_intent/RelativeLayout.java
@@ -2,13 +2,37 @@ package com.example.android_navigation_with_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RelativeLayout extends AppCompatActivity {
 
+    private EditText input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
+        input = (EditText) findViewById(R.id.input);
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult();
+        super.onBackPressed();
+    }
+
+    private void setResult(){
+        String name = input.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("userName", name);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void backToHomeBtnClicked(View view) {
+        setResult();
+        super.onBackPressed();
     }
 }
